864ed619ff64ff39bd5ae4fbf9e343ea45587ec5
alumno <profesorjosemanuelalvarez@gmail.com>
2026-01-20 19:22:57 +0100

    prueba

--- for_clases.sh
+++ for_clases.sh
@@ -4,6 +4,12 @@ set -u
 set -o pipefail
 
 
+alumnos() {
+for n in {1..30}; do
+    echo "alumno$1_$n"
+done
+}
+
 niveles_1eso() {
 # Definir el array
 niveles=("1esoA" "1esoB" "1esoC" "1esoD")
@@ -11,7 +17,9 @@ niveles=("1esoA" "1esoB" "1esoC" "1esoD")
 # for en la lista de cadenas de caracteres
 for nivel in "${niveles[@]}"; do
 #    echo "Nivel: $nivel"
+alumnos "$nivel"
 done
 }
 
-niveles
+#alumnos "1esoA"
+niveles_1eso
